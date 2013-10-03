1610af1fe80508522be7ddf2fdda290a5e09d862
David Fisher <tibbon@gmail.com>
2013-10-03 09:20:45 -0400

    Update quiz.rb

--- quiz.rb
+++ quiz.rb
@@ -12,8 +12,7 @@ heart = []
 
 while heart.empty?
   puts "i'll be all alone forever"
-  # binding.pry
-  heart << "love"
+  binding.pry
 end
 
 puts "There is hope for me!"
@@ -24,8 +23,7 @@ lion = ["cowardice", "fleas"]
 
 while lion.include? "cowardice"
   puts "what do they got that i ain't got?!"
-  # binding.pry
-  lion.shift
+  binding.pry
 end
 
 puts "Why, I'd thrash him from top to bottomus!"
@@ -36,8 +34,7 @@ head = ["niarb", "fluff"]
 
 until head.include? "brain"
   puts "oh i wish i could be of help"
-  # binding.pry
-  head[0].reverse!
+  binding.pry
 end
 
 puts "The sum of the squares of the legs of a right triangle is equal to the square of the hypotenuse"
